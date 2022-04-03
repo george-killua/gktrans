fcfb8dae0cb9423f5168833a1baace7290bb1bab
george-killua <george.kassih@gmx.at>
2022-04-03 12:13:06 +0200

    add suspend to block in dbTransaction

--- src/main/kotlin/com/killua/Application.kt
+++ src/main/kotlin/com/killua/Application.kt
@@ -2,10 +2,13 @@ package com.killua
 
 import io.ktor.application.*
 import com.killua.plugins.*
+import io.ktor.locations.*
+import io.ktor.server.engine.*
 
 fun main(args: Array<String>): Unit =
     io.ktor.server.netty.EngineMain.main(args)
 
+@KtorExperimentalLocationsAPI
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
     configureRouting()
@@ -14,4 +17,10 @@ fun Application.module() {
     configureSerialization()
     configureTemplating()
     configureMonitoring()
+    install(ShutDownUrl.ApplicationCallFeature) {
+        // The URL that will be intercepted
+        shutDownUrl = "/ktor/application/shutdown"
+        // A function that will be executed to get the exit code of the process
+        exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
+    }
 }
